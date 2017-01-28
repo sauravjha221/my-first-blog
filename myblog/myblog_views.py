1bab9655646d32efbeccbb00c4df982289018319
sauravjha221 <sauravjha221@gmail.com>
2017-01-28 20:46:54 +0530

    My myblog app, first commit

---
+++ myblog/myblog_views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+def index(request):
+	template = loader.get_template('homepage.html')
+	return HttpResponse(template.render(request))
